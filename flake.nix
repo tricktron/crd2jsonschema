03a6aeedf411fbdde4f08fd546710f0c08baf967
Thibault Gagnaux <tgagnaux@gmail.com>
2023-03-06 14:22:33 +0100

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    description               = "crd2jsonschema-dev-shell";
+    inputs.flake-utils.url    = "github:numtide/flake-utils";
+    inputs.nixpkgs.url        = "github:NixOS/nixpkgs";
+    inputs.my-system.url      = "github:tricktron/my-system-configs";
+
+    outputs = { self, nixpkgs, flake-utils, my-system }:
+    flake-utils.lib.eachSystem
+    [ 
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+    ]
+    (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            devShells.default     = pkgs.mkShell
+            {
+               packages = with pkgs;
+               [
+                   (bats.withLibraries (p: [ p.bats-support p.bats-assert p.bats-file ]))
+               ];
+            };
+        }
+    );
+}
