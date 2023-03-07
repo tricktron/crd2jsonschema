fee60e075073c7e97cdf644a3734db5d0d6edb75
Thibault Gagnaux <tgagnaux@gmail.com>
2023-03-07 15:45:52 +0100

    refactor: add function keywords and main function

--- src/crd2jsonschema.sh
+++ src/crd2jsonschema.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export WORKDIR
 
-cli_help() {
+function help() {
     echo "
 crd2jsonschema converts Kubernetes Custom Resource Definitions (CRDs) to JSON schema draft 4.
 Version: $(cat "$WORKDIR"/VERSION)
@@ -18,7 +18,7 @@ Available Commands:
   exit 1
 }
 
-convert_to_json()
+function convert_to_strict_json()
 {
     yq '.spec.versions.0.schema.openAPIV3Schema' "$1" | yq 'with(.. | 
         select(has("properties")) | 
@@ -27,14 +27,19 @@ convert_to_json()
         yq -o json -P
 }
 
-case "$1" in
+function run()
+{
+    case "$1" in
     "convert")
-        convert_to_json "$2"
+        convert_to_strict_json "$2"
         ;;
     "version")
         echo "crd2jsonschema version $(cat "$WORKDIR"/VERSION)"
         ;;
     *)
-        cli_help
+        help
         ;;
 esac
+}
+
+run "$@"
