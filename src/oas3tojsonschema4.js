3fafee58a30e9d5316aa6bbaea0627ac7c2516ca
Thibault Gagnaux <tgagnaux@gmail.com>
2023-10-24 15:06:59 +0900

    refactor: resolve breaking changes of dependency
    The openapi-schema-to-json-schema update from v3 -> v5 brought some
    breaking changes. This commit integrates the new v5 API.
    
    Besides, it puts the built dist/oas3tojsonschema4 binary in front of the
    path because otherwise it would take an old version of the
    oas3tojsonschema4 binary which might be installed on the system which is
    wrong because it does not contain the changes to test and might lead to
    strange behaviour: e.g. the tests work locally but would fail in the CI
    because the CI does not have the old oas3tojsonschema4 binary installed.
    As a result, we can again run the tests locally with `bats test` and be
    confident that we test the current changes and not and old version of
    the oas3tojsonschema4 binary.

--- src/oas3tojsonschema4.js
+++ src/oas3tojsonschema4.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
-
-const toJsonSchemaDraft4 = require("@openapi-contrib/openapi-schema-to-json-schema");
-const fs                 = require('fs');
+const fs                                                = require('fs');
+const { openapiSchemaToJsonSchema: toJsonSchemaDraft4 } = 
+    require("@openapi-contrib/openapi-schema-to-json-schema");
 
 try
 {
     const openapiV3Schema    = fs.readFileSync(0, 'utf-8');
-    const jsonSchemaDraft4   = toJsonSchemaDraft4(JSON.parse(openapiV3Schema.toString()));
+    const jsonSchemaDraft4   = toJsonSchemaDraft4
+    (
+        JSON.parse(openapiV3Schema.toString())
+    );
     console.log(JSON.stringify(jsonSchemaDraft4, null, 4));
     process.exit();
 
